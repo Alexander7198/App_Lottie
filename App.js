6183c985f8fc68c949f8d73c9d80a7a545aad113
Alexander Dorofeev <alexandedorofeev@yandex.ru>
2023-02-12 00:17:57 +0300

    one commit

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Lottie from 'lottie-react-native';
+import { SafeAreaView } from 'react-native';
+import poket from './assets/poket.json'
 
-export default function App() {
+export default function Animation() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Lottie resizeMode="contain" autoSize source={poket} autoPlay loop />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
